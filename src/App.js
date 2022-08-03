4ed2e20fb4feb64072743c4055ea79488cbf6b9d
Chadibz <91723882+Chadibz@users.noreply.github.com>
2022-08-04 00:48:53 +0100

    fertona

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import HeaderP from "./composant/Header/HeaderP";
+
 import "./style3.css";
 import "./style2.css";
 import "./style4.css";
 import "./style1.css";
-
+import "./style5.css";
 import SubHeader from "./composant/Header/SubHeader";
 import HomeP from "./composant/Home/HomeP";
 import FooterP from "./composant/Footer/FooterP";
 import SportvirtuelBanner from "./composant/Sportvirtuel/SportvirtuelBanner";
 import SportVirtuelContent from "./composant/Sportvirtuel/SportVirtuelContent";
 import SportvirtuelP from "./composant/Sportvirtuel/SportvirtuelP";
-
+import FentonaBanner from "./composant/Fentona/FentonaBanner";
+import FentonaP from "./composant/Fentona/FentonaP";
 function App() {
   return (
     <>
       <HeaderP></HeaderP>
       <SubHeader></SubHeader>
-      <SportvirtuelP></SportvirtuelP>
+      <FentonaP></FentonaP>
       <FooterP></FooterP>
     </>
   );
